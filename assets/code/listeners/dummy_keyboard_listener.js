b4999ac947f46cf923456267f9ac3d9b108a0d8f
Lastjuggernaut <vezina_jean@outlook.com>
2023-04-18 01:00:51 -0400

    V0.0.2
    First interaction with keyboard and app

---
+++ assets/code/listeners/dummy_keyboard_listener.js
@@ -0,0 +1,23 @@
+window.addEventListener("keydown", (clavier) => {
+
+console.log(clavier)
+if(clavier.key == "ArrowDown"){
+    //alert("ArrowDown")
+    moveDown()
+}
+if(clavier.key == "ArrowUp"){
+    //alert("ArrowDown")
+    moveUp()
+}
+if(clavier.key == "ArrowLeft"){
+    //alert("ArrowDown")
+    moveLeft()
+}
+if(clavier.key == "ArrowRight"){
+    //alert("ArrowDown")
+    moveRight()
+}
+if (clavier.key == "Enter"){
+    addDummy();
+}
+})
--- assets/code/testBasic.js
+++ assets/code/testBasic.js
@@ -1,5 +1,29 @@
 //Create a Pixi Application
 const app = new PIXI.Application({width: 256, height: 256});
-
+let coordx = 0
+let coordy = 0
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+function addDummy(){
+let sprite_test = PIXI.Sprite.from("assets/ressources/test.png")
+app.stage.addChild(sprite_test);
+sprite_test.x = coordx
+sprite_test.y = coordy  
+}
+
+function moveUp(){
+    coordy--
+}
+
+function moveDown(){
+    coordy++
+}
+
+function moveLeft(){
+    coordx--
+}
+
+function moveRight(){
+    coordx++
+}
