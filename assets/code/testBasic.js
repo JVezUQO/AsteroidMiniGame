237863d6728aa2959fa97c8c46adadaf69fde34e
JVezUQO <122834548+JVezUQO@users.noreply.github.com>
2023-04-30 02:14:17 -0400

    Better movement

--- assets/code/testBasic.js
+++ assets/code/testBasic.js
@@ -2,30 +2,45 @@
 const app = new PIXI.Application({width: 256, height: 256});
 let coordx = 128
 let coordy = 128
-//Add the canvas that Pixi automatically created for you to the HTML document
+let rotation = 0
+let movX = 0
+let movY = 0
+const ticker = new PIXI.Ticker();
+ticker.add(update)
+ticker.start();
+//Add the canvas that Pixi automatically created for you to the HTML document and create and append a test unit name sprite_test
 document.body.appendChild(app.view);
 const sprite_test = PIXI.Sprite.from("assets/ressources/test.png")
 app.stage.addChild(sprite_test);
+//
 
 
-app.ticker.add(delta => loop(delta));
-function loop(delta) {
-        
+
+function update(delta) {
+console.log(rotation, movX, movY)
+sprite_test.x += movX 
+sprite_test.y += movY
+sprite_test.rotation += rotation
 }
 
 
+sprite_test.pivot.x = 16
+sprite_test.pivot.y = 16
+
 function moveUp(){
-    sprite_test.y -= 1;
+    movX += 1;
 }
 
 function moveDown(){
-    sprite_test.y += 1;
+    movY += 1;
 }
 
 function moveLeft(){
-    sprite_test.x -= 1;
+    rotation -= 0.01;
+//    rotation = sprite_test.rotation
 }
 
 function moveRight(){
-    sprite_test.x += 1;
+    rotation += 0.01;
+//    rotation = sprite_test.rotation
 }
