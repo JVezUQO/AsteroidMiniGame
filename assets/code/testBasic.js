12d52300f2998eef9898ddfd1f1fb4c562982ba6
JVezUQO <122834548+JVezUQO@users.noreply.github.com>
2023-04-30 16:43:33 -0400

    Fully controllable player !

--- assets/code/testBasic.js
+++ assets/code/testBasic.js
@@ -1,5 +1,5 @@
 //Create a Pixi Application
-const app = new PIXI.Application({width: 256, height: 256});
+const app = new PIXI.Application({width: 512, height: 512});
 let coordx = 128
 let coordy = 128
 let rotation = 0
@@ -10,17 +10,19 @@ ticker.add(update)
 ticker.start();
 //Add the canvas that Pixi automatically created for you to the HTML document and create and append a test unit name sprite_test
 document.body.appendChild(app.view);
-const sprite_test = PIXI.Sprite.from("assets/ressources/test.png")
+const sprite_test = PIXI.Sprite.from("assets/ressources/player.png")
 app.stage.addChild(sprite_test);
-//
+sprite_test.position.x = coordx
+sprite_test.position.y = coordy
 
-
+// Main program
 
 function update(delta) {
-console.log(rotation, movX, movY)
+console.log(rotation, movX, movY, rotation*180/(2*Math.PI))
 sprite_test.x += movX 
 sprite_test.y += movY
 sprite_test.rotation += rotation
+OOB()
 }
 
 
@@ -28,19 +30,36 @@ sprite_test.pivot.x = 16
 sprite_test.pivot.y = 16
 
 function moveUp(){
-    movX += 1;
+    movY -= Math.cos(sprite_test.rotation);
+    movX += Math.sin(sprite_test.rotation);
 }
 
 function moveDown(){
-    movY += 1;
+    movY += Math.cos(sprite_test.rotation);
+    movX -= Math.sin(sprite_test.rotation);
 }
 
 function moveLeft(){
-    rotation -= 0.01;
+    rotation -= 0.005;
 //    rotation = sprite_test.rotation
 }
 
 function moveRight(){
-    rotation += 0.01;
+    rotation += 0.005;
 //    rotation = sprite_test.rotation
+}
+
+function OOB(){ //Out of Bound
+    if(sprite_test.position.x >= 513){
+        sprite_test.position.x = 0        
+    }
+    if(sprite_test.position.y >= 512){
+        sprite_test.position.y = 0        
+    }
+    if(sprite_test.position.x <= -1){
+        sprite_test.position.x = 512        
+    }
+    if(sprite_test.position.y <= -1){
+        sprite_test.position.y = 512       
+    }
 }
